5a0c612cb89608b07329b00c72e60e19ac322b20
Atchaya23 <atchaya.tirougnaname@edu.esiee.fr>
2022-12-07 17:45:21 +0100

    Project

--- project-1/src/main.c
+++ project-1/src/main.c
@@ -32,6 +32,11 @@
 #include "timer.h"          // Timer library for AVR-GCC
 #include <lcd.h>            // Peter Fleury's LCD library
 #include <stdlib.h>         // C library. Needed for number conversions
+#include "joystick_definitions.h" 
+
+static int var_x = 0;
+static int var_y = 1; 
+
 
 
 
@@ -44,46 +49,278 @@
  * Returns:  none
  **********************************************************************/
 int main(void)
-{
-    /*
+{   
+
+    // Initialize display
     lcd_init(LCD_DISP_ON);
-    lcd_gotoxy(0,0);
-    lcd_puts("HI");
+    //lcd_gotoxy(1, 0); lcd_puts("Y:");
+    //lcd_gotoxy(1, 1); lcd_puts("X:");
 
-    lcd_gotoxy(0,0);
-    if(analogRead(X_pin) == 513){
-    lcd_puts("STOPPED");
-    } 
-    else if(analogRead(X_pin) < 513) {
-    lcd_puts("REVERSE");
-    } 
-    else if(analogRead(X_pin) > 513) {
-    lcd_puts("FORWARD");
-    }
-    
-    lcd_gotoxy(0, 1);
-    if(analogRead(X_PIN) == 512){
-    lcd_puts("STOPPED");
-    } 
-    else if(analogRead(X_PIN) < 512) {
-    lcd_puts("LEFT ");
-    } 
-    else if(analogRead(X_PIN) > 512) {
-    lcd_puts("RIGHT");
-    }
-    
 
-    int var = analogRead(X_PIN);
-    printf(var);
-    */
+    
+    lcd_gotoxy(var_x, var_y); lcd_puts("I");
+    //lcd_gotoxy(10, 1); lcd_puts("O");
+
+
+    lcd_command(1<<LCD_CGRAM);       // Set addressing to CGRAM (Character Generator RAM)
+                                     // ie to individual lines of character patterns
+    /*for (uint8_t i = 0; i < 8; i++)  // Copy new character patterns line by line to CGRAM
+        lcd_data(customChar[i]);
+    */lcd_command(1<<LCD_DDRAM);       // Set addressing back to DDRAM (Display Data RAM)
+                                     // ie to character codes
+    
+   
+
+    // Display symbol with Character code 0
+    
+    lcd_putc(0x00);
+    // Initialize display
+    //lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+
+    // Put string(s) on LCD screen
+    //lcd_gotoxy(6, 1);
+    //lcd_puts("LCD Test");
+    //lcd_putc('!');
+
+    //question 7 
+    //lcd_clrscr();
+    /*lcd_gotoxy(1,0);
+    lcd_puts("00:00.0");
+    lcd_gotoxy(11,0);
+    lcd_putc('a');
+    lcd_gotoxy(1,1);
+    lcd_puts("b");
+    lcd_gotoxy(11,1);
+    lcd_puts("c");
+    */// set black light at PB2
+    GPIO_mode_output(&DDRB, PB2);
+    GPIO_write_high(&PORTB,PB2);
+
+    // Configure Analog-to-Digital Convertion unit
+    // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
+    ADMUX = ADMUX | (1<<REFS0);
+    // Select input channel ADC0 (voltage divider pin)
+    ADMUX = ADMUX & ~(1<<MUX3 | 1<<MUX2 | 1<<MUX1 | 1<<MUX0); //here we are clearing the four bits
+    // Enable ADC module
+    ADCSRA = ADCSRA | (1<<ADEN);
+    // Enable conversion complete interrupt
+    ADCSRA = ADCSRA | (1<<ADIE);
+    // Set clock prescaler to 128
+    ADCSRA = ADCSRA | (1<<ADPS2 | 1<<ADPS1 | 1<<ADPS0);
+    // Configuration of 8-bit Timer/Counter2 for Stopwatch update
+    // Set the overflow prescaler to 16 ms and enable interrupt
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+
+/* Interrupt service routines ----------------------------------------*/
+
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Use single conversion mode and start conversion every 100 ms.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+    // Start ADC conversion
+    ADCSRA = ADCSRA | (1<<ADSC);
+}
+
+
+/**********************************************************************
+ * Function: ADC complete interrupt
+ * Purpose:  Display converted value on LCD screen.
+ **********************************************************************/
+ISR(ADC_vect)
+{
     uint16_t value;
-    char string[4];
+    
+  
+
+
+    char string[4];  // String for converted numbers by itoa()
+    static int channel = 0;
     value = ADC;
 
-    itoa(value, string, 10);
-    lcd_gotoxy(8,0);
-    lcd_puts("  ");
-    lcd_gotoxy(8,0);
-    lcd_puts(string);
+  if (channel==1){
+    channel =0;
+  }else{
+    channel=1;
+  }
 
+    switch (channel)
+    {
+    case 1:
+      ADMUX = ADMUX | 1<<MUX0;
+
+      itoa(value, string, 10);
+      lcd_gotoxy(3,0);
+      //lcd_puts(string);
+
+        if( value > 530){
+        lcd_clrscr();
+        lcd_gotoxy(1, 0);
+        lcd_puts("DOWN");
+        //lcd_gotoxy(10, 1); lcd_puts("O");
+        /*if(var_x=10){
+          lcd_clrscr();
+          lcd_gotoxy(1, 0);
+          lcd_puts("LOST");
+        }else{*/
+        lcd_gotoxy(var_x, 1);
+        lcd_puts("I");
+        //}
+    
+
+    }
+    /*
+    if (value <530 && value>500){
+        lcd_gotoxy(2, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(13, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(7, 1);
+        //lcd_puts("BUTTON");
+    }
+    */
+    
+        if( value < 500){
+        lcd_clrscr();
+        lcd_gotoxy(1, 0);
+        lcd_puts("UP");
+        //lcd_gotoxy(10, 1); lcd_puts("O");
+        if(var_x>4){
+          lcd_gotoxy(var_x, 0);
+          lcd_puts("I");
+        }else{
+          lcd_gotoxy(var_x, var_y);
+          lcd_puts("I");
+        }
+    }
+      break;
+    case 0:
+      ADMUX = ADMUX & ~(1<<MUX3 | 1<<MUX2 | 1<<MUX1 | 1<<MUX0);
+
+      itoa(value, string, 10);
+      lcd_gotoxy(3,1);
+      //lcd_puts(string);
+
+        if( value > 505){
+        lcd_clrscr();
+        lcd_gotoxy(1, 0);
+        lcd_puts("LEFT");
+        //lcd_gotoxy(10, 1); lcd_puts("O");
+        /*if(var_x=11){
+          lcd_clrscr();
+          lcd_gotoxy(1, 0);
+          lcd_puts("LOST");
+        }else{*/
+        lcd_gotoxy(var_x-1, var_y);
+        lcd_puts("I");
+        //}
+        var_x=var_x-1;
+
+    }
+    if( value < 480){
+
+        lcd_clrscr();
+        
+        lcd_gotoxy(1, 0);
+        lcd_puts("RIGHT");
+        //lcd_gotoxy(10, 1); lcd_puts("O");
+        /*if(var_x=9){
+          lcd_clrscr();
+          lcd_gotoxy(1, 0);
+          lcd_puts("LOST");
+        }else{*/
+        lcd_gotoxy(var_x+1, var_y);
+        lcd_puts("I");
+        //}
+        var_x+=1;
+
+
+    }
+
+    if(var_x>15){
+      lcd_clrscr();
+      lcd_gotoxy(1, 0);
+      lcd_puts("WIN");
+    }
+
+    }
+
+
+    
+    // Read converted value
+    // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
+    
+    // Convert "value" to "string" and display it
+
+
+    //print the decimal value converted into hexa
+    /*
+    itoa(channel, string, 10);
+    lcd_gotoxy(13,0);
+    lcd_puts("   ");
+    lcd_gotoxy(13, 0);
+    lcd_puts(string);
+  */
+
+
+
+    
+    /*switch (xvalue)
+    {
+      case (520 < xvalue):
+        lcd_gotoxy(8, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(8, 1);
+        lcd_puts("none");
+        break;
+      case 640:
+        lcd_gotoxy(8, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(8, 1);
+        lcd_puts("SELECT");
+        break;
+      case 409:
+        lcd_gotoxy(8, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(8, 1);
+        lcd_puts("LEFT");
+        break;
+      case 257:
+        lcd_gotoxy(8, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(8, 1);
+        lcd_puts("DOWN");
+        break;
+      case 99:
+        lcd_gotoxy(8, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(8, 1);
+        lcd_puts("UP");
+        break;
+      case 0:
+        lcd_gotoxy(8, 1);
+        lcd_puts("      ");
+        lcd_gotoxy(8, 1);
+        lcd_puts("RIGHT");
+        break;
+    }
+    */
 }
+
